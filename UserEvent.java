ae0afc7f6c0c50aed5f60839f6bdbf79056a4867
Manuel Di Nardo <134846139+Ciomino@users.noreply.github.com>
2024-06-12 17:59:56 +0200

    Add files via upload

--- UserEvent.java
+++ UserEvent.java
@@ -1,13 +1,11 @@
 package com.example.FinalProject;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class UserEvent {
@@ -15,7 +13,36 @@ public class UserEvent {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(mappedBy="utente")
-    private Set<UserEvent> userEvents = new HashSet<>();
+    @ManyToOne
+    @JoinColumn(name = "utente_id")
+    private Utente utente;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Evento evento;
+
+    
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public Evento getEvento() {
+        return evento;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+    public void setEvento(Evento evento) {
+        this.evento = evento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    
     
 }
